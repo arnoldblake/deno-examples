719e92d9eeca266d411208d36aa84940bff899c1
Blake Arnold <barnold@blakearnold.me>
2025-02-23 13:52:14 -0500

    Created AppHeader component

---
+++ with-nuxt/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+  const { user, clear: clearSession } = useUserSession();
+
+  async function logout() {
+    await clearSession();
+    await navigateTo("/login");
+  }
+</script>
+
+<template>
+  <nav class="p-4 bg-gray-100">
+    <NuxtLink to="/" class="text-blue-600 hover:text-blue-800">
+      Dinosaur Encyclopedia
+    </NuxtLink>
+    <div>
+      <h1>Welcome {{ user?.name }}</h1>
+      <button @click="logout">Logout</button>
+    </div>
+  </nav>
+</template>
